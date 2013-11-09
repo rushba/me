419700b9d2348bbddfc11a0351583d9df9e4b73a
rushba <novdmitry@gmail.com>
2013-11-09 22:42:26 +0400

    cleanup & org & habitrpg

--- init.el
+++ init.el
@@ -1,34 +1,3 @@
-(unless (featurep 'cedet)
-  (when (file-directory-p "~/.emacs.d/el-get/cedet")
-    (progn
-      (add-to-list 'load-path  "~/.emacs.d/el-get/cedet")
-      (load-file "~/.emacs.d/el-get/cedet/cedet-devel-load.el"))))
-;;; ede
-(setq semantic-default-submodes
-      '(;; cache(?)
-        global-semanticdb-minor-mode
-
-        global-semantic-highlight-edits-mode
-        global-semantic-idle-local-symbol-highlight-mode
-        ;; global-cedet-m3-minor-mode
-
-        ;; code helpers
-        global-semantic-idle-scheduler-mode
-        global-semantic-idle-summary-mode
-        global-semantic-idle-completions-mode
-
-        ;; eye candy
-        global-semantic-decoration-mode
-        global-semantic-highlight-func-mode
-        global-semantic-highlight-edits-mode
-        global-semantic-stickyfunc-mode
-
-        ;; debugging semantic itself
-        ;;global-semantic-show-parser-state-mode 1   ;; show the parsing state in the mode line
-        ;;global-semantic-show-unmatched-syntax-mode 1
-        ))
-
-(global-ede-mode 1)
 (setq me-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 (require 'org)
 (org-babel-load-file (expand-file-name "me.org" me-dir))
